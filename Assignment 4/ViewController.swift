5da0ce0b257d1bc3fe22dc641f44a3cddc80332f
Macmini-13 <macmini-13@Macmini-13s-Mac-mini.local>
2022-09-13 09:38:46 +0545

    Swift assignment 4 login

---
+++ Assignment 4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 4
+//
+//  Created by Macmini-13 on 12/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
